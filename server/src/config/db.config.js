872a4e47555c51b7e8fb5030d523e566181c4aee
Dhanesh <dhanesh2435@gmail.com>
2025-10-06 12:15:57 +0530

    Initial commit of coworking space project (Local to Github)

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -2,11 +2,7 @@ import 'dotenv/config';
 import {Pool} from 'pg';
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
