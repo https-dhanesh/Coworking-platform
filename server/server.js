cd9797f6be5379a5b8432abc6575ab53b26d246e
Dhanesh <dhanesh2435@gmail.com>
2025-10-06 15:10:48 +0530

    chore: final configurations for deployment

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ import cors from 'cors'
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL 
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use('/api/auth',authRoutes);
