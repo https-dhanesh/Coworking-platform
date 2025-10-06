22012352979e94bbf5a334fa84b8d170bdab2c97
Dhanesh <dhanesh2435@gmail.com>
2025-10-06 12:48:52 +0530

    feat: Prepare frontend for deployment

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { RegisterFormData, LoginFormData, CreateSpaceFormData } from '../types';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
 });
 
 export const registerUser = async (data: RegisterFormData) => {
